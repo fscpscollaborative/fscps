98f76d30f5f2766a7b90cd2bc92d802ab7935565
Oleksandr Nikolaiev <remixod@gmail.com>
2022-09-09 19:04:47 +0200

    fix

--- RunPipeline/RunPipeline.ps1
+++ RunPipeline/RunPipeline.ps1
@@ -133,8 +133,9 @@ try {
     $msReferencePath = "$($buildPath)\$($settings.nugetPackagesPath)\$tools_package".Trim()
     $msOutputDirectory = "$($buildPath)\bin".Trim()
 
-    $tempFile = (Get-Content $PackagesConfigFileName).Replace('ReferenceFolders', $msReferenceFolder)
-    Set-Content $NewPackagesFile $tempFile
+    $buidPropsFile = Join-Path $buildPath NewBuild\Build\build.props
+    $tempFile = (Get-Content $buidPropsFile).Replace('ReferenceFolders', $msReferenceFolder)
+    Set-Content $buidPropsFile $tempFile
 
     msbuild NewBuild\Build\Build.sln  `
          /p:BuildTasksDirectory=$msBuildTasksDirectory `
