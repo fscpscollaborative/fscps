e651c98416d80c452e98c45857fb185a67603d44
Oleksandr Nikolaiev <remixod@gmail.com>
2022-09-09 11:14:52 +0200

    update

--- RunPipeline/RunPipeline.ps1
+++ RunPipeline/RunPipeline.ps1
@@ -45,24 +45,33 @@ try {
     }
 
     #Generate solution folder
+    Write-Host "========Generate solution folder"
     GenerateSolution -ModelName $settings.models -NugetFeedName $settings.nugetFeedName -NugetSourcePath $settings.nugetSourcePath -DynamicsVersion $DynamicsVersion
 
-
+    Write-Host "========Cleanup Build folder"
     #Cleanup Build folder
     Remove-Item $settings.buildPath -Recurse -Force -ErrorAction SilentlyContinue
 
+    Write-Host "========Copy branch files"
     #Copy branch files
     New-Item -ItemType Directory -Force -Path $settings.buildPath; Copy-Item $ENV:GITHUB_WORKSPACE\* -Destination $settings.buildPath -Recurse -Force
 
+    Write-Host "========Copy solution folder"
     #Copy solution folder
-    Copy-Item .\NewBuild -Destination $settings.buildPath
+    Copy-Item NewBuild -Destination $settings.buildPath
 
+    Write-Host "========Cleanup NuGet"
     #Cleanup NuGet
     nuget sources remove -Name $settings.nugetFeedName -Source $settings.nugetSourcePath
 
+    Write-Host "========Nuget add source"
     #Nuget add source
     nuget sources Add -Name $settings.nugetFeedName -Source $settings.nugetSourcePath -username $secrets.AF_CONNECTORS_CICD_USER -password $secrets.AF_CONNECTORS_CICD_PASS
    
+
+
+    Write-Host "Found packages.config file" $settings.buildPath\NewBuild\packages.config
+    Write-Host "========Nuget install packages"
     #Nuget install packages
     nuget restore $settings.buildPath\NewBuild\packages.config -PackagesDirectory $settings.buildPath\NuGets
 
