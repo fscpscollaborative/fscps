06282c1abaf814ae28a302a8b89ab026c3dd779d
Oleksandr Nikolaiev <remixod@gmail.com>
2022-09-09 12:49:50 +0200

    Add build

--- RunPipeline/RunPipeline.ps1
+++ RunPipeline/RunPipeline.ps1
@@ -19,6 +19,28 @@ try {
     . (Join-Path -Path $PSScriptRoot -ChildPath "..\FnSCM-Go-Helper.ps1" -Resolve)
 
   
+
+    $VersionsFile = Join-Path $ENV:GITHUB_WORKSPACE '.FnSCM-Go\versions.json'
+
+    $versions = (Get-Content $VersionsFile) | ConvertFrom-Json
+
+    Foreach($version in $versions)
+    {
+        if($version.version -eq $DynamicsVersion)
+        {
+            $PlatformVersion = $version.data.PlatformVersion
+            $ApplicationVersion = $version.data.AppVersion
+        }
+    }
+
+
+    $tools_package = Join-Path 'Microsoft.Dynamics.AX.Platform.CompilerPackage.' $PlatformVersion
+    $plat_package = Join-Path 'Microsoft.Dynamics.AX.Platform.DevALM.BuildXpp.' $PlatformVersion
+    $app_package = Join-Path 'Microsoft.Dynamics.AX.Application.DevALM.BuildXpp.' $ApplicationVersion
+    $appsuite_package = Join-Path 'Microsoft.Dynamics.AX.ApplicationSuite.DevALM.BuildXpp.' $ApplicationVersion
+
+
+
     $runAlPipelineParams = @{}
     $project = "" 
     $baseFolder = Join-Path $ENV:GITHUB_WORKSPACE $project
@@ -28,7 +50,9 @@ try {
     }
     $workflowName = $env:GITHUB_WORKFLOW
 
-    Write-Host "use settings and secrets"
+    #Use settings and secrets
+    Write-Host "======================================== Use settings and secrets"
+
     $settings = $settingsJson | ConvertFrom-Json | ConvertTo-HashTable
     $secrets = $secretsJson | ConvertFrom-Json | ConvertTo-HashTable
     $appBuild = $settings.appBuild
@@ -49,33 +73,32 @@ try {
     }
 
     #Generate solution folder
-    Write-Host "========Generate solution folder"
+    Write-Host "======================================== Generate solution folder"
     GenerateSolution -ModelName $settings.models -NugetFeedName $settings.nugetFeedName -NugetSourcePath $settings.nugetSourcePath -DynamicsVersion $DynamicsVersion
 
-    Write-Host "========Cleanup Build folder"
+    Write-Host "======================================== Cleanup Build folder"
     #Cleanup Build folder
     Remove-Item $settings.buildPath -Recurse -Force -ErrorAction SilentlyContinue
 
-    Write-Host "========Copy branch files"
+    Write-Host "======================================== Copy branch files"
     #Copy branch files
     New-Item -ItemType Directory -Force -Path $settings.buildPath; Copy-Item $ENV:GITHUB_WORKSPACE\* -Destination $settings.buildPath -Recurse -Force
 
-    Write-Host "========Copy solution folder"
+    Write-Host "======================================== Copy solution folder"
     #Copy solution folder
     Copy-Item NewBuild -Destination $settings.buildPath -Recurse -Force
 
-    Write-Host "========Cleanup NuGet"
+    Write-Host "======================================== Cleanup NuGet"
     #Cleanup NuGet
     nuget sources remove -Name $settings.nugetFeedName -Source $settings.nugetSourcePath
 
-    Write-Host "========Nuget add source"
+    Write-Host "======================================== Nuget add source"
     #Nuget add source
     nuget sources Add -Name $settings.nugetFeedName -Source $settings.nugetSourcePath -username $nugetFeedUserSecretName -password $nugetFeedPasswordSecretName
    
-
     $packagesFilePath = Join-Path $settings.buildPath NewBuild\packages.config
     
-    Write-Host "========Nuget install packages"
+    Write-Host "======================================== Nuget install packages"
 
     if(Test-Path $packagesFilePath)
     {
@@ -91,6 +114,23 @@ try {
     nuget restore -PackagesDirectory ..\NuGets
 
     cd $settings.buildPath
+
+    #Build solution
+    Write-Host "======================================== Build solution"
+
+
+
+
+
+    msbuild NewBuild\Build\Build.sln  
+         /p:BuildTasksDirectory="$settings.buildPath\$settings.nugetPackagesPath\$tools_package\DevAlm" 
+         /p:MetadataDirectory="$settings.buildPath\$settings.metadataPath" 
+         /p:FrameworkDirectory="$settings.buildPath\$settings.nugetPackagesPath\$tools_package" 
+         /p:ReferencePath="$settings.buildPath\$settings.nugetPackagesPath\$tools_package" 
+         /p:OutputDirectory="$settings.buildPath\bin"
+         --% /p:ReferenceFolder="$settings.buildPath\$settings.nugetPackagesPath\$app_package\ref\net40;$settings.buildPath\$settings.nugetPackagesPath\$plat_package\ref\net40;$settings.buildPath\$settings.nugetPackagesPath\$appsuite_package\ref\net40;$settings.buildPath\$settings.metadataPath;$settings.buildPath\bin"
+
+
 
 
 
