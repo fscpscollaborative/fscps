d1acf9036466e9cf092df1e3566d4f7c962f1a11
Oleksandr Nikolaiev <remixod@gmail.com>
2022-09-12 13:06:45 +0200

    fix

--- FnSCM-Go-Helper.ps1
+++ FnSCM-Go-Helper.ps1
@@ -1817,6 +1817,48 @@ function Get-FullNormalizedPath {
 
     $outPath
 }
+
+function ConvertTo-OrderedDictionary
+{
+    #requires -Version 2.0
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [hashtable]
+        $InputObject,
+
+        [Type]
+        $KeyType = [string]
+    )
+
+    process
+    {
+        #$outputObject = New-Object "System.Collections.Generic.Dictionary[[$($KeyType.FullName)],[Object]]"
+        $outputObject = New-Object "System.Collections.Specialized.OrderedDictionary"
+
+        foreach ($entry in $InputObject.GetEnumerator())
+        {
+            $newKey = $entry.Key -as $KeyType
+            
+            if ($null -eq $newKey)
+            {
+                throw 'Could not convert key "{0}" of type "{1}" to type "{2}"' -f
+                      $entry.Key,
+                      $entry.Key.GetType().FullName,
+                      $KeyType.FullName
+            }
+            elseif ($outputObject.Contains($newKey))
+            {
+                throw "Duplicate key `"$newKey`" detected in input object."
+            }
+
+            $outputObject.Add($newKey, $entry.Value)
+        }
+
+        Write-Output $outputObject
+    }
+}
 ################################################################################
 # End - Private functions.
 ################################################################################
