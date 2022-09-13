c1cdf64ae4337d597648a1b16e7138c057c734de
Oleksandr Nikolaiev <remixod@gmail.com>
2022-09-13 19:07:50 +0200

    update

--- ReadSettings/ReadSettings.ps1
+++ ReadSettings/ReadSettings.ps1
@@ -36,38 +36,7 @@ try {
         $settings.doNotSignApps = $true
     }
 
-    $outSettings = @{}
-    $getSettings | ForEach-Object {
-        $setting = $_.Trim()
-        $outSettings += @{ "$setting" = $settings."$setting" }
-        Add-Content -Path $env:GITHUB_ENV -Value "$setting=$($settings."$setting")"
-    }
-
-    $outSettingsJson = $outSettings | ConvertTo-Json -Compress
-    Write-Host "::set-output name=SettingsJson::$outSettingsJson"
-    Write-Host "set-output name=SettingsJson::$outSettingsJson"
-    Add-Content -Path $env:GITHUB_ENV -Value "Settings=$OutSettingsJson"
-
-    $gitHubRunner = $settings.githubRunner.Split(',') | ConvertTo-Json -compress
-    Write-Host "::set-output name=GitHubRunnerJson::$githubRunner"
-    Write-Host "set-output name=GitHubRunnerJson::$githubRunner"
-
-
-
-    if($settings.buildVersion.Contains(','))
-    {
-        $versionsJSon = $settings.buildVersion.Split(',') | ConvertTo-Json -compress
-        Write-Host "::set-output name=VersionsJson::$versionsJSon"
-        Write-Host "set-output name=VersionsJson::$versionsJSon"
-        Add-Content -Path $env:GITHUB_ENV -Value "Versions=$versionsJSon"
-    }
-    else
-    {
-        $versionsJSon = '["'+$($settings.buildVersion).ToString()+'"]'
-        Write-Host "::set-output name=VersionsJson::$versionsJSon"
-        Write-Host "set-output name=VersionsJson::$versionsJSon"
-        Add-Content -Path $env:GITHUB_ENV -Value "Versions=$versionsJSon"
-    }
+    
 
     $EnvironmentsFile = Join-Path $ENV:GITHUB_WORKSPACE '.FnSCM-Go\environments.json'
     $envsFile = (Get-Content $EnvironmentsFile) | ConvertFrom-Json
@@ -82,6 +51,10 @@ try {
             {
                 if($env.name -eq $dynamicsEnvironment)
                 {
+                    if($env.settings.PSobject.Properties.name -match "deploy")
+                    {
+                        $env.settings.deploy = $true
+                    }
                     MergeCustomObjectIntoOrderedDictionary -dst $settings -src $env.settings
                 }
             }
@@ -106,13 +79,53 @@ try {
     }
     else    
     {
-        $environments = @($envsFile | ForEach-Object { $_.Name })
+        $environments = @($envsFile | ForEach-Object { 
+            $check = $true
+            if($_.settings.PSobject.Properties.name -match "deploy")
+            {
+                $check = $_.settings.deploy
+            }
+            if($check)
+            {
+                $_.Name
+            }
+        })
         $environmentsJSon = $environments | ConvertTo-Json -compress
         Write-Host "::set-output name=EnvironmentsJson::$environmentsJson"
         Write-Host "set-output name=EnvironmentsJson::$environmentsJson"
         Add-Content -Path $env:GITHUB_ENV -Value "Environments=$environmentsJson"
     }
 
+    $outSettings = @{}
+    $getSettings | ForEach-Object {
+        $setting = $_.Trim()
+        $outSettings += @{ "$setting" = $settings."$setting" }
+        Add-Content -Path $env:GITHUB_ENV -Value "$setting=$($settings."$setting")"
+    }
+
+    $outSettingsJson = $outSettings | ConvertTo-Json -Compress
+    Write-Host "::set-output name=SettingsJson::$outSettingsJson"
+    Write-Host "set-output name=SettingsJson::$outSettingsJson"
+    Add-Content -Path $env:GITHUB_ENV -Value "Settings=$OutSettingsJson"
+
+    $gitHubRunner = $settings.githubRunner.Split(',') | ConvertTo-Json -compress
+    Write-Host "::set-output name=GitHubRunnerJson::$githubRunner"
+    Write-Host "set-output name=GitHubRunnerJson::$githubRunner"
+
+    if($settings.buildVersion.Contains(','))
+    {
+        $versionsJSon = $settings.buildVersion.Split(',') | ConvertTo-Json -compress
+        Write-Host "::set-output name=VersionsJson::$versionsJSon"
+        Write-Host "set-output name=VersionsJson::$versionsJSon"
+        Add-Content -Path $env:GITHUB_ENV -Value "Versions=$versionsJSon"
+    }
+    else
+    {
+        $versionsJSon = '["'+$($settings.buildVersion).ToString()+'"]'
+        Write-Host "::set-output name=VersionsJson::$versionsJSon"
+        Write-Host "set-output name=VersionsJson::$versionsJSon"
+        Add-Content -Path $env:GITHUB_ENV -Value "Versions=$versionsJSon"
+    }
 }
 catch {
     OutputError -message $_.Exception.Message
