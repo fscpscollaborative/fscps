f15c0dbd28813c550a0b1a4dc2312a1516661031
Oleksandr Nikolaiev <remixod@gmail.com>
2022-09-13 13:29:57 +0200

    update

--- FnSCM-Go-Helper.ps1
+++ FnSCM-Go-Helper.ps1
@@ -96,7 +96,7 @@ function OutputInfo {
         [string]$Message
     )
         filter timestamp {"[$(Get-Date -Format yyyy:MM:dd-HH:MM:ss)]: $_"}
-        Write-PSFHostColor -DefaultColor Green  ($Message | timestamp)
+        Write-FormattedOutput -ForegroundColor Green  ($Message | timestamp)
 }
 function OutputDebug {
     Param(
@@ -110,6 +110,38 @@ function OutputDebug {
         Write-Host "::Debug::$message"
     }
 }
+
+function Write-FormattedOutput
+{
+    [CmdletBinding()]
+    Param(
+         [Parameter(Mandatory=$True,Position=1,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)][Object] $Object,
+         [Parameter(Mandatory=$False)][ConsoleColor] $BackgroundColor,
+         [Parameter(Mandatory=$False)][ConsoleColor] $ForegroundColor
+    )    
+
+    # save the current color
+    $bc = $host.UI.RawUI.BackgroundColor
+    $fc = $host.UI.RawUI.ForegroundColor
+
+    # set the new color
+    if($BackgroundColor -ne $null)
+    { 
+       $host.UI.RawUI.BackgroundColor = $BackgroundColor
+    }
+
+    if($ForegroundColor -ne $null)
+    {
+        $host.UI.RawUI.ForegroundColor = $ForegroundColor
+    }
+
+    Write-Output $Object
+  
+    # restore the original color
+    $host.UI.RawUI.BackgroundColor = $bc
+    $host.UI.RawUI.ForegroundColor = $fc
+}
+
 
 function GetUniqueFolderName {
     Param(
