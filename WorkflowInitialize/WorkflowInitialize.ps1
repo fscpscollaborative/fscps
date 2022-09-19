8a94b1749ffdbfb0ef7f65db7008f2b0f54f58c1
Oleksandr Nikolaiev <remixod@gmail.com>
2022-09-19 09:08:38 +0200

    ipd

--- WorkflowInitialize/WorkflowInitialize.ps1
+++ WorkflowInitialize/WorkflowInitialize.ps1
@@ -8,6 +8,8 @@ Set-StrictMode -Version 2.0
 try {
 
 
+    $github = (Get-ActionContext).Payload
+    Write-Host ($github | ConvertTo-Json)
 
     $ap = "$ENV:GITHUB_ACTION_PATH".Split('\')
     $branch = $ap[$ap.Count-2]
