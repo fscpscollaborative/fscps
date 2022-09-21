137e559a534cedf6acb969cc8399af6742775c42
Oleksandr Nikolaiev <remixod@gmail.com>
2022-09-21 09:37:12 +0200

    upd

--- RunPipeline/RunCommercePipeline.ps1
+++ RunPipeline/RunCommercePipeline.ps1
@@ -29,7 +29,7 @@ try {
     $secrets = $secretsJson | ConvertFrom-Json | ConvertTo-HashTable
 
     $settingsHash = $settings #| ConvertTo-HashTable
-    'nugetFeedPasswordSecretName','nugetFeedUserSecretName','lcsUsernameSecretname','lcsPasswordSecretname','azClientsecretSecretname','repoTokenSecretName' | ForEach-Object {
+    $settings.secretsList | ForEach-Object {
         $setValue = ""
         if($settingsHash.Contains($_))
         {
