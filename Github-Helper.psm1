65c027584f52b70382cbab88ef677c0a26ae9489
Oleksandr Nikolaiev <remixod@gmail.com>
2022-09-27 12:32:07 +0200

    fix

--- Github-Helper.psm1
+++ Github-Helper.psm1
@@ -808,3 +808,80 @@ function Test-RangeAndValue($AtomContraint, $NowValue) {
 }
 
 
+function Update-SessionEnvironment {
+<#
+.SYNOPSIS
+Updates the environment variables of the current powershell session with
+any environment variable changes that may have occured during a
+Chocolatey package install.
+.DESCRIPTION
+When Chocolatey installs a package, the package author may add or change
+certain environment variables that will affect how the application runs
+or how it is accessed. Often, these changes are not visible to the
+current PowerShell session. This means the user needs to open a new
+PowerShell session before these settings take effect which can render
+the installed application nonfunctional until that time.
+Use the Update-SessionEnvironment command to refresh the current
+PowerShell session with all environment settings possibly performed by
+Chocolatey package installs.
+.NOTES
+This method is also added to the user's PowerShell profile as
+`refreshenv`. When called as `refreshenv`, the method will provide
+additional output.
+Preserves `PSModulePath` as set by the process starting in 0.9.10.
+.INPUTS
+None
+.OUTPUTS
+None
+#>
+
+  Write-FunctionCallLogMessage -Invocation $MyInvocation -Parameters $PSBoundParameters
+
+  $refreshEnv = $false
+  $invocation = $MyInvocation
+  if ($invocation.InvocationName -eq 'refreshenv') {
+    $refreshEnv = $true
+  }
+
+  if ($refreshEnv) {
+    Write-Output 'Refreshing environment variables from the registry for powershell.exe. Please wait...'
+  } else {
+    Write-Verbose 'Refreshing environment variables from the registry.'
+  }
+
+  $userName = $env:USERNAME
+  $architecture = $env:PROCESSOR_ARCHITECTURE
+  $psModulePath = $env:PSModulePath
+
+  #ordering is important here, $user should override $machine...
+  $ScopeList = 'Process', 'Machine'
+  if ('SYSTEM', "${env:COMPUTERNAME}`$" -notcontains $userName) {
+    # but only if not running as the SYSTEM/machine in which case user can be ignored.
+    $ScopeList += 'User'
+  }
+  foreach ($Scope in $ScopeList) {
+    Get-EnvironmentVariableNames -Scope $Scope |
+        ForEach-Object {
+          Set-Item "Env:$_" -Value (Get-EnvironmentVariable -Scope $Scope -Name $_)
+        }
+  }
+
+  #Path gets special treatment b/c it munges the two together
+  $paths = 'Machine', 'User' |
+    ForEach-Object {
+      (Get-EnvironmentVariable -Name 'PATH' -Scope $_) -split ';'
+    } |
+    Select-Object -Unique
+  $Env:PATH = $paths -join ';'
+
+  # PSModulePath is almost always updated by process, so we want to preserve it.
+  $env:PSModulePath = $psModulePath
+
+  # reset user and architecture
+  if ($userName) { $env:USERNAME = $userName; }
+  if ($architecture) { $env:PROCESSOR_ARCHITECTURE = $architecture; }
+
+  if ($refreshEnv) {
+    Write-Output 'Finished'
+  }
+}
