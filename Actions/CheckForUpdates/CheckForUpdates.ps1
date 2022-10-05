a73a5c5a41665942a42ddf89eb31f653200c1518
Oleksandr Nikolaiev <remixod@gmail.com>
2022-10-05 11:06:56 +0200

    fic

--- Actions/CheckForUpdates/CheckForUpdates.ps1
+++ Actions/CheckForUpdates/CheckForUpdates.ps1
@@ -341,6 +341,7 @@ try {
                 
                 if (!$directcommit) {
                     $branch = [System.IO.Path]::GetRandomFileName()
+                    invoke-git fetch --all
                     invoke-git checkout -b $branch $targetBranch
                 }
 
