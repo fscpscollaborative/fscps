62335c89b1517a525b54a96864920a382dce4da1
Oleksandr Nikolaiev <remixod@gmail.com>
2022-10-18 22:46:06 +0200

    update

--- Actions/ReadSettings/ReadSettings.ps1
+++ Actions/ReadSettings/ReadSettings.ps1
@@ -72,7 +72,7 @@ try {
 
         if($dynamicsEnvironment.Split(","))
         {
-            $environmentsJSon = $dynamicsEnvironment | ConvertTo-Json -compress
+            $environmentsJSon = '["'+$dynamicsEnvironment+'"]'
         }
         else
         {
