6f722d4a9795355f4a28b3f6bebb168263912bdb
onikolaiev <remixod@gmail.com>
2022-11-09 08:55:00 +0100

    Fix

--- Actions/WorkflowPostProcess/WorkflowPostProcess.ps1
+++ Actions/WorkflowPostProcess/WorkflowPostProcess.ps1
@@ -62,51 +62,45 @@ try {
         Write-Host ($runsActiveParams | ConvertTo-Json)
         $runsActive = Invoke-RestMethod @runsActiveParams
         $actions = $runsActive.workflow_runs
-        [array]$baseURIJobs = @()
+        [array]$deleteRuns = @()
 
         foreach ($action in $actions) {
-            $del = $false
-            #if run older than 7 days - delete
-            $retentionHours = (7 * 24)
+        
             $timeSpan = NEW-TIMESPAN -Start $action.created_at -End (Get-Date).ToString()
-            if ($timeSpan.TotalHours -gt $retentionHours) {
-                $del = $true
+            $del = $false
+            if ($timeSpan.Days -gt 7) {
+              $del = $true
             }
-            #if it`s a clean deploy run - delete
-            if($action.display_title -match "DEPLOY" -and ($action.status -eq "completed") -and $remove)
+        
+            if($action.display_title -match "DEPLOY" -and ($action.status -eq "completed"))
             {
-                $del = $true
+                $getJobsParam = @{
+                    Uri     = $action.jobs_url
+                    Method  = "Get"
+                    Headers = $baseHeader
+                } 
+                #Delete skipped actions
+                $jobs = Invoke-RestMethod @getJobsParam
+                foreach($job in $jobs.jobs){
+                    if($job.conclusion -eq "skipped"){$del = $true}
+                }
             }
-
+        
             if($del)
             {
                 Write-Host "Found job $($action.display_title)"
-                $baseURIJobs += ("/repos/{0}/actions/runs/{1}" -f $githubRepository, $action.id)
+                $deleteRuns += $action
             }
         }
-        foreach ($baseURIJob in $baseURIJobs) {
-            $delete = $false
-            $getJobsParam = @{
-                Uri     = ( "{0}{1}/jobs" -f $uriBase, $baseURIJob )
-                Method  = "Get"
-                Headers = $baseHeader
-            } 
+        foreach ($action in $deleteRuns) {
 
-            $jobs = Invoke-RestMethod @getJobsParam
-            foreach($job in $jobs.jobs){
-                if($job.conclusion -eq "skipped"){$delete = $true}
-            }
-
-            $runsDeleteParam = @{
-                Uri     = ( "{0}{1}" -f $uriBase, $baseURIJob )
-                Method  = "Delete"
-                Headers = $baseHeader
-            } 
-            if($delete)
-            {
+                $runsDeleteParam = @{
+                    Uri     = $action.html_url
+                    Method  = "Delete"
+                    Headers = $baseHeader
+                } 
                 Write-Host "Delete job $(($runsDeleteParam.Uri -split "/")[8])"
                 Invoke-RestMethod @runsDeleteParam
-            }
         }
     }
 }
