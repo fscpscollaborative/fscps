47052c4dfb03582023270562b4883e61069042f6
Oleksandr Nikolaiev <oleksandr.nikolaiev@ciellos.com>
2022-11-15 08:51:03 +0100

    update

--- Actions/Helpers/Github-Helper.psm1
+++ Actions/Helpers/Github-Helper.psm1
@@ -214,7 +214,8 @@ function invoke-choco {
     )
     begin
     {
-        If(!(Get-Command -Name choco.exe -ErrorAction SilentlyContinue) -or !(Test-Path "C:\ProgramData\chocolatey"))
+        #!(Get-Command -Name choco.exe -ErrorAction SilentlyContinue) -or 
+        If(!(Test-Path "C:\ProgramData\chocolatey"))
         {
             Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))  
         }
