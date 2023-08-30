492dbbd9092a6c04f0be5f49b36f065215db41dc
Oleksandr Nikolaiev <oleksandr.nikolaiev@ciellos.com>
2023-08-30 12:43:40 +0300

    upd

--- Actions/FSC-PS-Helper.ps1
+++ Actions/FSC-PS-Helper.ps1
@@ -286,7 +286,9 @@ function Get-AXReferencedTestModel
     { 
         $mdlName = $dr.BaseName
         Write-Host "ModelName: $mdlName"
-        if(-not ($mdlName -contains ("Test"))){ return;}
+        $checkTest = $($mdlName -contains ("Test"))
+        if(-not $checkTest){ return; }
+        Write-Host "Contains test: $checkTest"
         $descriptorSearchPath = (Join-Path $dr.FullName "Descriptor")
         Write-Host "DescriptorSearchPath: $descriptorSearchPath"
         $descriptor = (Get-ChildItem -Path $descriptorSearchPath -Filter '*.xml')
