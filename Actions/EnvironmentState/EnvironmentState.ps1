7cf420d9f671bd6ed91ff7af689e0f0a60dabfdf
Oleksandr Nikolaiev <oleksandr.nikolaiev@ciellos.com>
2023-10-04 20:51:13 +0300

    fix

--- Actions/EnvironmentState/EnvironmentState.ps1
+++ Actions/EnvironmentState/EnvironmentState.ps1
@@ -85,7 +85,7 @@ try {
     if ($AzureRMAccount) { 
         #Do Logic
         OutputInfo "== Logged in == $($settings.azTenantId) "
-
+        az vm list
         OutputInfo "Getting Azure VM State $($settings.azVmname)"
         $PowerState = ([string](az vm list -d --query "[?name=='$($settings.azVmname)'].powerState").Trim().Trim("[").Trim("]").Trim('"').Trim("VM ")).Replace(' ','')
         OutputInfo "....state is $($PowerState)"
