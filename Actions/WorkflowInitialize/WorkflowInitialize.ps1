145fffd4bbd00125802581acaff3a2270daad32b
Oleksandr Nikolaiev <oleksandr.nikolaiev@ciellos.com>
2023-10-17 20:12:34 +0300

    fix

--- Actions/WorkflowInitialize/WorkflowInitialize.ps1
+++ Actions/WorkflowInitialize/WorkflowInitialize.ps1
@@ -27,13 +27,7 @@ try {
     }
 
     #Test-ALGoRepository -baseFolder $ENV:GITHUB_WORKSPACE
-    installModules @("AZ.Storage","d365fo.tools")
-
-
-
-
-
-
+    #installModules @("AZ.Storage","d365fo.tools")
 }
 catch {
     OutputError -message $_.Exception.Message
