972723c6e6575fbaa47f22d88987845599d70b61
Oleksandr Nikolaiev <oleksandr.nikolaiev@ciellos.com>
2023-10-30 21:54:42 +0200

    fix

--- Actions/WorkflowInitialize/WorkflowInitialize.ps1
+++ Actions/WorkflowInitialize/WorkflowInitialize.ps1
@@ -7,17 +7,19 @@ Set-StrictMode -Version 2.0
 try {
     Import-Module (Join-Path $PSScriptRoot "..\FSC-PS-Helper.ps1")
     Import-Module (Join-Path $PSScriptRoot "..\Helpers\ReadSecretsHelper.psm1")
+    OutputInfo "Getting GitHub Context ..."
     $github = (Get-ActionContext)
     Write-Host ($github | ConvertTo-Json)
 
     $ap = "$ENV:GITHUB_ACTION_PATH".Split('\')
     $branch = $ap[$ap.Count-2]
 
+    OutputInfo "Installing PSSodium..."
     Install-Module -Name PSSodium -Force
     Write-Big -str "$branch"
 
     #Load REPO_TOKEN secret from github
-    $github = (Get-ActionContext)
+    
     try {
         $ghToken = GetSecret -secret "REPO_TOKEN"
         if(!$ghToken){throw "GitHub secret REPO_TOKEN not found. Please, create it."}
